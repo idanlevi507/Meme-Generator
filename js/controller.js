10d57aad86631893ac3767376cf30bb3bf3ac690
idanlevi507 <idanlevi15@gmail.com>
2021-04-20 11:37:09 +0300

    canvas start

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,56 @@
 'use strict';
 
+var gCanvas;
+var gCtx;
+
+
+
 function init() {
     gImgs = loadImages();
     renderImages();
 }
 
 
-function renderImages() {
-
+function renderImages() { 
     let imgs = getgImgs();
-    console.log(imgs);
     let htmlImgs = imgs.map(img => {
-        return `<img src="${img.url}" onclick="onOpenCanvas(this)>`
+        return `<img src="${img.url}" onclick="onImgClick(${img.id})">`
     })
     htmlImgs = htmlImgs.join('');
     document.querySelector(".gallery-images").innerHTML = htmlImgs;
 }
 
-function onOpenCanvas(el) {
+function onInputText(value) {   
+    setTextinput(value)
+}
 
+function setCanvImg() {
+    const currMeme = getCurrMeme();
+    gCanvas.style = `background-image: url("meme-imgs/${currMeme.selectedImgId}.jpg");background-size: cover;`
+}
+
+function renderCanvas() {
+    const htmlCanvas = `<canvas id="my-canvas" width="475px" height="450px"> </canvas>`;
+    document.querySelector(".canvas-container").innerHTML = htmlCanvas;
+    gCanvas = document.querySelector('#my-canvas');
+    gCtx = gCanvas.getContext("2d");
+
+}
+
+function openMemeEditor(id) {
+    setSelectedImg(id);
+    renderCanvas(id)  
+    setCanvImg();
+    document.querySelector(".modal-content").style.display = "flex";
+    drawText()
+}
+
+function onImgClick(id) {
+    document.querySelector(".gallery-container").style.display = 'none';
+    openMemeEditor(id);
+}
+
+function onGallery() {
+    document.querySelector(".modal-content").style.display = "none";
+    document.querySelector(".gallery-container").style.display = 'block';
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -9,12 +9,40 @@ let gMeme = {
             txt: 'I never eat Falafel',
             size: 20,
             align: 'left',
-            color: 'red'
+            color: 'red',
+            family: 'IMPACT',
+            x:50,
+            y:50,
         }
     ]
 }
 
+function setTextinput(value) {
+    gMeme.lines.txt = value;
+    console.log(gMeme);
+    onImgClick(gMeme.selectedImgId);
+}
 
+function drawText() {
+    gMeme.lines.forEach(line => {
+        gCtx.lineWidth = 3;
+        gCtx.strokeStyle = 'white';
+        gCtx.fillStyle = line.color;
+        gCtx.font = '40px lato-reg';
+        gCtx.textAlign = line.align;
+        gCtx.fillText(line.txt,line.x,line.y);
+    })
+}
+
+
+function getCurrMeme() {
+    return gMeme
+}
+
+function setSelectedImg(id) {
+    gMeme.selectedImgId = id
+    console.log(gMeme, id);
+}
 
 function loadImages() {
     let imgs = []
